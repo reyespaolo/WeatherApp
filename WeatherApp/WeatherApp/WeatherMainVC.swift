6e75a578b0bd91112204a1be0ea3f8872ed47064
Paolo Reyes <reyes.paolo@360logix.com>
2017-03-24 11:06:42 +0800

    Create Interface

--- WeatherApp/WeatherApp/WeatherMainVC.swift
+++ WeatherApp/WeatherApp/WeatherMainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherMainVC.swift
 //  WeatherApp
 //
 //  Created by Paolo Reyes on 23/03/2017.
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class WeatherMainVC: UIViewController {
-
+class WeatherMainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Required Delegate Methods for Table View
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        return cell
+    }
+    
+    //End
 }
 
